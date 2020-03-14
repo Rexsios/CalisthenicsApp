caf6adc2c8857900f58671eb0d8b90f7df928acd
Slawomir Palka <rexsiomag@gmail.com>
2020-03-14 14:07:52 +0100

    Initial commit

---
+++ src/Containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+
+import { Container, Row, Col } from 'react-bootstrap'
+
+function App() {
+  return (
+    <Container>
+      <Row></Row>
+    </Container>
+  );
+}
+
+export default App;
